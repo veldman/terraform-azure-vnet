0c3f3c74936428d348b1dfcd3507a27f2e661371
Sergio Sisternes <sesispla@outlook.com>
2019-12-25 22:14:18 +0100

    Added default address_space

--- variables.tf
+++ variables.tf
@@ -16,6 +16,7 @@ variable resource_group {
 variable address_space {
   type        = list(string)
   description = "(Required) The address space that is used the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
+  address_space  = ["10.0.0.0/16"]
 }
 
 variable dns_servers {
