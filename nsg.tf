4b6d531214172abbe8b04a86230567bf60bff3f7
Sergio Sisternes <sesispla@outlook.com>
2019-12-26 01:29:41 +0100

    Interpolation refactoring

--- nsg.tf
+++ nsg.tf
@@ -1,12 +1,12 @@
 resource "azurerm_network_security_group" "cloudcommons" {
   count               = var.nsg_enabled == true ? 1 : 0
   name                = "${var.app}-vnet-nsg"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+  location            = var.location
+  resource_group_name = var.resource_group
 
   tags = {
-    app         = "${var.app}"
-    environment = "${var.environment}"
-    creator     = "${var.creator}"
+    app         = var.app
+    environment = var.environment
+    creator     = var.creator
   }
 }
--- vnet.tf
+++ vnet.tf
@@ -5,8 +5,8 @@ locals {
 
 resource "azurerm_virtual_network" "cloudcommons" {
   name                = "${var.name}-vnet"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+  location            = var.location
+  resource_group_name = var.resource_group
   address_space       = var.address_space
   dns_servers         = var.dns_servers
 
@@ -28,8 +28,8 @@ resource "azurerm_virtual_network" "cloudcommons" {
   }
 
   tags = {
-    app         = "${var.app}"
-    environment = "${var.environment}"
-    creator     = "${var.creator}"
+    app         = var.app
+    environment = var.environment
+    creator     = var.creator
   }
 }
