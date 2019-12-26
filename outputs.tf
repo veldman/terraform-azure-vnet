92a6bf90b379bec11f47512c017b905de50ce76c
Sergio Sisternes <sesispla@outlook.com>
2019-12-26 02:14:10 +0100

    Added subnets to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   value       = "${azurerm_virtual_network.cloudcommons.id}"
   description = "VNET Id"
 }
+
+output "subnets" {
+    value     = azurerm_virtual_network.cloudcommons.subnet
+    description = "Subnet"
+}
